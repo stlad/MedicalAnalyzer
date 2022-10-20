0abbbf7e8030a67acc1fa748d7d19d452cbf0d4f
stlad <82213506+stlad@users.noreply.github.com>
2022-10-20 19:42:07 +0500

    переделка проекта. Работа с postgresql

--- DB_Module/db_module.py
+++ DB_Module/db_module.py
@@ -1,27 +1,34 @@
 import psycopg2
 
+class DB_user():
+    def __init__(self,database = 'MedicalAnalysis_DB', user = 'postgres', password='admin', host='localhost', port='5432'):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        print('User Entered successfully!')
 
-def create_connection_to_DB(database, user, password, host, port):
-    '''Создает соединение с базоый данных и возвращает его'''
-    try:
-        con = psycopg2.connect(
-            database=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        print("Database opened successfully!")
-        return con
-    except psycopg2.OperationalError:
-        print('Не получилось подключиться к базе данных')
-        return
+    def create_connection_to_DB(self):
+        '''Создает соединение с базоый данных и возвращает его'''
+        try:
+            con = psycopg2.connect(
+                database=self.database,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+            #print("Database opened successfully!")
+            return con
+        except psycopg2.OperationalError:
+            print('Не получилось подключиться к базе данных')
+            return
 
 
+def date_text_to_sql_format(text):
+    splitted_text = text.split('.')
+    return f'{splitted_text[2]}-{splitted_text[1]}-{splitted_text[0]}'
 
-class DB_controller:
-    def __init__(self):
-        self.con = create_connection_to_DB('MedicalAnalysis_DB', 'postgres', 'admin', 'localhost', '5432')
 
-
-CON = DB_controller()
+MainBDUser = DB_user()
