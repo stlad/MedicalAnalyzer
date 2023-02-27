cd21a1622a93bb1d32d3f58eb0b060bd4b6b7f23
stlad <82213506+stlad@users.noreply.github.com>
2023-02-28 01:45:57 +0500

    Вывод отчета об анализе в docx

--- FunctionalModules/Report_Module/DocxReports.py
+++ FunctionalModules/Report_Module/DocxReports.py
@@ -18,34 +18,39 @@ class DocxReporter:
 
     def _get_table_from_template(self):
         doc = Document('FunctionalModules/Report_Module/form_template.docx')
-        return deepcopy(doc.tables[0])
+        #return deepcopy(doc.tables[0])
+        return doc.tables[0]
 
 
     def _get_classic_table(self):
-        template_table = self._get_table_from_template()
-        doc = Document()
-        paragraph = doc.add_paragraph()
-        paragraph._p.addnext(template_table._tbl)
+        doc = Document('FunctionalModules/Report_Module/form_template.docx')
 
         tbl = doc.tables[0]
 
         tbl.rows[0].cells[1].text = f"{self.patient.surname} {self.patient.name} {self.patient.patronymic} "
         tbl.rows[1].cells[1].text = self.patient.gender # ПОЛ
         tbl.rows[2].cells[1].text = date_sql_to_text_format(str(self.analysis.analysis_date)) # ДАТА АНАЛИЗА
-        tbl.rows[3].cells[1].text = self.patient.get_age_by_date(self.analysis.analysis_date) + ' Полных лет'# ВОЗРАСТ
+        tbl.rows[3].cells[1].text = f'{self.patient.get_age_by_date(self.analysis.analysis_date)} Полных лет'# ВОЗРАСТ
         tbl.rows[4].cells[1].text = self.patient.diag # ДИАГНОЗ 1
         tbl.rows[5].cells[1].text = self.patient.second_diag # ДИАГНОЗ 2
 
         tbl.rows[6].cells[1].text = self.patient.genes  # ГЕНЫ 1,2,3
         tbl.rows[7].cells[1].text = self.patient.genes  # ГЕНЫ 1,2,3
         tbl.rows[8].cells[1].text = self.patient.genes  # ГЕНЫ 1,2,3
-
         tbl.rows[9].cells[1].text = '0' if self.analysis.analysis_date.month in [3,4,5,6,7,8] else '1'
+
+        self._fill_params(tbl)
         return doc
 
 
     def _fill_params(self, tbl):
-        i = 0
+        i=10
+        for param in self.analysis.parameters:
+            if i==10 or i == 19 or i == 30 :
+                i+=1
+            tbl.rows[i].cells[0].text = param.name
+            tbl.rows[i].cells[1].text = str(param.value)
+            i+=1
 
 
     def save_to_file(self, filename):
